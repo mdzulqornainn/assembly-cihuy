ddba97e133e6ec6bed258ed293ccb23c13c7e797
Muhammad Dzulqornain <dzenmori@gmail.com>
2026-01-10 23:24:26 +0700

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,9 +18,7 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`
-        fixed top-0 left-0 w-full z-50
-        transition-all duration-300
+      className={`font-press fixed top-0 left-0 w-full z-50 transition-all duration-300
         ${show ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"}
         bg-white/80 backdrop-blur shadow
       `}
