fd07ec0ed0bb0816760deb139309f63acc988ff8
Muhammad Dzulqornain <dzenmori@gmail.com>
2026-01-23 11:48:58 +0700

    add foto

--- src/data/Profiles.jsx
+++ src/data/Profiles.jsx
@@ -1,9 +1,28 @@
 // data/profiles.js
-import profile1 from "../assets/profile.jpg";
+// import profile1 from "../assets/profile/1.jpg";
+// import profile2 from "../assets/profile/2.jpg";
+// import profile3 from "../assets/profile/3.jpg";
+import profile4 from "../assets/profile/4.jpg";
+import profile5 from "../assets/profile/5.jpg";
+// import profile6 from "../assets/profile/6.jpg";
+// import profile7 from "../assets/profile/7.jpg";
+import profile8 from "../assets/profile/8.jpg";
+import profile9 from "../assets/profile/9.jpg";
+// import profile10 from "../assets/profile/10.jpg";
+import profile11 from "../assets/profile/11.jpg";
+import profile12 from "../assets/profile/12.jpg";
 
 export const profiles = [
-  { id: 1, name: "Andi", img: profile1 },
-  { id: 2, name: "Budi", img: "/profile2.jpg" },
-  { id: 3, name: "Citra", img: "/profile3.jpg" },
-  { id: 4, name: "Dewi", img: "/profile4.jpg" },
+  // { id: 1, name: "Andi", img: profile1 },
+  // { id: 2, name: "Budi", img: profile2 },
+  // { id: 3, name: "Citra", img: profile3 },
+  { id: 4, name: "Farrel", img: profile4 },
+  { id: 5, name: "Aldi", img: profile5 },
+  // { id: 6, name: "Fajar", img: profile6 },
+  // { id: 7, name: "Gina", img: profile7 },
+  { id: 8, name: "Brilian", img: profile8 },
+  { id: 9, name: "Marisya", img: profile9 },
+  // { id: 10, name: "Joko", img: profile10 },
+  { id: 11, name: "Audi", img: profile11 },
+  { id: 12, name: "Nain/Dzul/Jul  ", img: profile12 },
 ];
