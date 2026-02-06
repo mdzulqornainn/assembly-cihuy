b530bb8e7b7c257da5c91b686a8bfd042eae0d40
Muhammad Dzulqornain <dzenmori@gmail.com>
2026-02-06 09:39:54 +0700

    fix gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Member from "./pages/Member.jsx";
-import Gallery from "./pages/Gallery.jsx";
+// import Gallery from "./pages/Gallery.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/member" element={<Member />} />
-      <Route path="/gallery" element={<Gallery />} />
+      {/* <Route path="/gallery" element={<Gallery />} /> */}
     </Routes>
   );
 }
