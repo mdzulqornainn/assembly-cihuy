f1c47f885528b1d851a5c54e7dc6fd84fd58670a
Muhammad Dzulqornain <dzenmori@gmail.com>
2026-02-13 16:59:38 +0700

    load fix

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import pixelCharacter from "../assets/char.svg";
-// pakai gambar pixel mario/karakter 8bit kamu
 
 export default function GameLoading({ onFinish }) {
   const [progress, setProgress] = useState(0);
@@ -24,8 +23,8 @@ export default function GameLoading({ onFinish }) {
   }, [onFinish]);
 
   return (
-    <div className="fixed inset-0 bg-white flex flex-col items-center justify-center overflow-hidden">
-      {/* Character Jump Animation */}
+    <div className="fixed inset-0 bg-white flex font-press flex-col items-center justify-center overflow-hidden">
+      {/* Character Jump */}
       <motion.img
         src={pixelCharacter}
         alt="pixel character"
@@ -39,12 +38,11 @@ export default function GameLoading({ onFinish }) {
       />
 
       {/* Text */}
-      <h1 className="text-yellow-400 text-2xl font-mono tracking-wider mb-8 animate-pulse">
+      <h1 className="text-blue-600 text-2xl font-mono tracking-wider mb-8 animate-pulse">
         Tunggu bentar yaa....
       </h1>
 
-      {/* Pixel Loading Bar */}
-      <div className="flex gap-2">
+      <div className="flex gap-2 mb-6">
         {Array.from({ length: 10 }).map((_, index) => (
           <motion.div
             key={index}
@@ -53,14 +51,21 @@ export default function GameLoading({ onFinish }) {
               backgroundColor: progress / 10 > index ? "#facc15" : "#1f2937",
             }}
             animate={progress / 10 > index ? { scale: [1, 1.2, 1] } : {}}
-            transition={{
-              duration: 0.3,
-            }}
+            transition={{ duration: 0.2 }}
           />
         ))}
       </div>
 
-      {/* Ground */}
+      {/* Persentase */}
+      <motion.p
+        className="text-black  text-lg"
+        key={progress}
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.2 }}
+      >
+        {progress}%
+      </motion.p>
     </div>
   );
 }
