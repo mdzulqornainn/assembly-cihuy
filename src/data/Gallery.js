e4f882084d65f0a2c3a1a3cacb2abc68a599424b
Muhammad Dzulqornain <dzenmori@gmail.com>
2026-05-20 08:04:17 +0700

    update gallery, tambah foto baru, dan update data gallery

--- src/data/Gallery.js
+++ src/data/Gallery.js
@@ -14,6 +14,15 @@ import gallery13 from "../assets/gallery/13.jpg";
 import gallery14 from "../assets/gallery/14.jpg";
 import gallery15 from "../assets/gallery/15.jpg";
 import gallery16 from "../assets/gallery/16.jpg";
+import gallery17 from "../assets/gallery/17.jpg";
+import gallery18 from "../assets/gallery/18.jpg";
+import gallery19 from "../assets/gallery/19.jpg";
+import gallery20 from "../assets/gallery/20.jpg";
+import gallery21 from "../assets/gallery/21.jpg";
+import gallery22 from "../assets/gallery/22.jpg";
+import gallery23 from "../assets/gallery/23.jpg";
+import gallery24 from "../assets/gallery/24.jpg";
+import gallery25 from "../assets/gallery/25.jpg";
 
 export const galleryItems = [
   { id: 1, img: gallery1, alt: "Moment 1" },
@@ -32,4 +41,13 @@ export const galleryItems = [
   { id: 14, img: gallery14, alt: "Moment 14" },
   { id: 15, img: gallery15, alt: "Moment 15" },
   { id: 16, img: gallery16, alt: "Moment 16" },
+  { id: 17, img: gallery17, alt: "Moment 17" },
+  { id: 18, img: gallery18, alt: "Moment 18" },
+  { id: 19, img: gallery19, alt: "Moment 19" },
+  { id: 20, img: gallery20, alt: "Moment 20" },
+  { id: 21, img: gallery21, alt: "Moment 21" },
+  { id: 22, img: gallery22, alt: "Moment 22" },
+  { id: 23, img: gallery23, alt: "Moment 23" },
+  { id: 24, img: gallery24, alt: "Moment 24" },
+  { id: 25, img: gallery25, alt: "Moment 25" },
 ];
